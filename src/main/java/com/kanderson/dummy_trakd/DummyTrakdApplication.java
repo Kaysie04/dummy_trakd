befef15105578471ce0bad4e9c80fa83c9756c8d
Kaysie Anderson <kaysielandersonwork@gmail.com>
2023-02-07 18:10:43 -0600

    first commit

---
+++ src/main/java/com/kanderson/dummy_trakd/DummyTrakdApplication.java
@@ -0,0 +1,13 @@
+package com.kanderson.dummy_trakd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyTrakdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyTrakdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanderson/dummy_trakd/DummyTrakdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanderson.dummy_trakd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyTrakdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
